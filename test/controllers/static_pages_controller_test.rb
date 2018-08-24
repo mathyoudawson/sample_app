d03160bd47832db32a47b5e5e63e77cf0527fc73
Matt Dawson <matt.dawson@fluxfederation.com>
2018-08-24 12:15:04 +1200

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", @default_title
   end
 
-
   test "should get help" do
     get help_path
     assert_response :success
