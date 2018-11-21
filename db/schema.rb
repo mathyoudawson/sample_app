bb7be0bbaa55b97ee667e146fbf7e8ff2efb8bce
Matthew D <matthewdawson@outlook.com>
2018-11-21 16:26:39 +1300

    Handle case sensitivity for users on model and db level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_002142) do
+ActiveRecord::Schema.define(version: 2018_11_21_031504) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
